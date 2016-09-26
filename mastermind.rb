b3c0c016a9215d594060164ab4de63d56b34f4a5
Henry Harding McCall <hardingmccall@gmail.com>
2016-09-26 16:52:03 -0500

    refactored and completed guess verification (check_guess)

--- mastermind.rb
+++ mastermind.rb
@@ -7,45 +7,38 @@ class Mastermind
 		@answer = []
 		@correctness = ['-', '-', '-', '-']
 		@colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo']
-		@number_colors_in_answer = {'red' => 0, 'orange' => 0, 'yellow' => 0,'green' => 0,'blue' => 0,'indigo' => 0}
+		@number_colors_in_answer = {'red' => 0, 'orange' => 0, 'yellow' => 0,'green' => 0,'blue' => 0,'indigo' => 0, 'guessed' => 0}
+		self.robo_answer
 	end
 
 	def robo_answer
 		4.times do |i|
 			@answer[i] = @colors.sample
 		end
-		@answer = ['red','red','indigo','blue']
 		@answer.each do |x|
 			@colors.each do |y|
 				@number_colors_in_answer[y] += 1 if y==x
 			end
 		end
+		print @answer
+		puts " "
 	end
 
 	def check_guess(guess)
-		# puts "Invalid guess"; return "no" if guess.length != 4
-		temp_guess = guess
-		temp_number_colors = @number_colors_in_answer
-		first_check_correct_color = 0
-		correct_location = 0
-		#first check for the number of correct colors
-		puts temp_number_colors[guess[1]]
-		@colors.each do |x|
-			for i in 0...temp_guess.length do
-				puts "hey"; first_check_correct_color += 1; temp_guess.delete_at(i); temp_number_colors[x] -= 1 if temp_number_colors[guess[i]] > 0
-			end
+		print "You guessed: #{guess}\n"
+		correct_color, correct_position, temp_number_colors = 0, 0, @number_colors_in_answer
+		for i in 0...guess.length do
+			(correct_position += 1; temp_number_colors[guess[i]] -= 1; guess[i] = "guessed") if guess[i] == @answer[i]
 		end
-		puts temp_guess
-		puts temp_number_colors
-		puts first_check_correct_color
-
+		for i in 0...guess.length do
+			(correct_color += 1; temp_number_colors[guess[i]] -= 1) if temp_number_colors[guess[i]] > 0
+		end
+		puts "# of correct colors in wrong position: #{correct_color}\n# of colors in correct position: #{correct_position}"
 	end
 end
 
 		
 test123 = Mastermind.new("joe")
+bowdown = ['blue','red','green','red']
 
-test123.robo_answer
-guess = ['red','red','orange','indigo']
-puts guess.length
-test123.check_guess(guess)
+test123.check_guess(bowdown)
